46d2c2501fef0ab7157c06dc6cefef7f9d98ab74
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-11-04 19:09:37 -0500

    This is the beginning to the project called Road Rage that our team will be developing.

---
+++ src/roadrage/RoadRage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadrage;
+
+/**
+ *
+ * @author markymark1346
+ */
+public class RoadRage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
