47ff4e28068a622177011593f7314f7c7f86e8ac
cboucher13 <caroline.r.boucher@gmail.com>
2015-11-04 20:53:16 -0500

    HighScores

---
+++ src/roadrage/HighScores.java
@@ -0,0 +1,37 @@
+
+
+package RoadRage;
+
+import java.io.*;
+
+public class HighScores
+{
+    public static void high (String newName)
+    {
+        try
+        {
+            String verify, putData;
+            File file = new File("scores.txt");
+            String name = newName;
+            file.createNewFile();
+            //FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+            bw.write(name);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            
+            while( ( line = br.readLine()) != null)
+            {
+                System.out.println(line);
+            } 
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
