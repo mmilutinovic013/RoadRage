a8aba31f69ffa39d24d8c82a00ce7662e76d4187
sab5964 <sab5964@206-L29.up.ist.local>
2015-11-11 19:42:12 -0500

    Merge origin/master
    Conflicts:
    	TDGame/src/tdgame/gamePanel.java

--- src/roadrage/Board.java
+++ src/roadrage/Board.java
@@ -2,6 +2,7 @@ package roadrage;
 
 import java.awt.*;
 import java.awt.event.*;
+
 /*
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,15 +14,17 @@ import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.Rectangle;
 
+
 public class Board extends JFrame implements ActionListener {
-    
+    int y = 0;
+    int x = 0;
     //TODO: We need Desk and Enemy baefore we can do Board
     // GridLayout? 
     // Push
     private JLabel map;
     private JPanel gameboardPanel;
     private JPanel sidebarPanel;
-    private JButton tempButton; // This is testing if the sidebar Panel exists
+    private JButton tempButton; // This is testing if the sidebar Panel exist
 
     public Board() {
         
@@ -53,8 +56,61 @@ public class Board extends JFrame implements ActionListener {
     
     public void actionPerformed(ActionEvent evt){
         
-        //action events
         Object obj = evt.getSource();
     }
-}
+ 
+  // trying to get objects to fall
+    
+    
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        x=0;
+        y=0;
+        int objmove;
+
+// This creates the object 
+            Image img1 = Toolkit.getDefaultToolkit().getImage("obstacle.png");
+            g2.drawImage(img1,180,180,this);
+            g2.finalize();
+
+// this will randomly select the lane (1-3)
+            
+            int lane =  (int) Math.ceil ((Math.random() * 2)+ 1);
+// this creates the starting location  of the object randomly
+            if(lane == 1)
+                {
+                    x= 100;
+                }
+            if(lane == 2)
+                {
+                    x= 300;
+                }
+            if(lane == 3)
+                {
+                    x = 500;
+                }
+            
+   // This makes the object fall     
+        // for( objmove = 0; objmove++; objmove < 100)
+         {
+             g.drawImage(img1,x,y,null);
+             y--;
+             try
+             {
+              Thread.sleep(250);
+             }
+             
+              catch (InterruptedException e)
+             {
+                 e.printStackTrace();
+             }
+         }
+            
+         }
+     
+        
+        
+    }
+
     
